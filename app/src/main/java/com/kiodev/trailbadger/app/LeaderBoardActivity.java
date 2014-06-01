97a81367fce6e82e4017ff2ba4bc0042870f2742
Kelly Shuster <kelly.shuster@gmail.com>
2014-06-01 09:37:17 -0600

    leaderboards are fancy with imgs

--- app/src/main/java/com/kiodev/trailbadger/app/LeaderBoardActivity.java
+++ app/src/main/java/com/kiodev/trailbadger/app/LeaderBoardActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -148,6 +149,9 @@ public class LeaderBoardActivity extends ListActivity {
             TextView elevTextView = (TextView) convertView.findViewById(R.id.height);
             DecimalFormat formatter = new DecimalFormat("#,###");
             elevTextView.setText( formatter.format(u.getFeet()) );
+
+            ImageView userImageView = (ImageView) convertView.findViewById(R.id.ImagView_user);
+            userImageView.setImageResource(u.getImgResId());
 
             return convertView;
         }
--- app/src/main/java/com/kiodev/trailbadger/app/User.java
+++ app/src/main/java/com/kiodev/trailbadger/app/User.java
@@ -1,12 +1,25 @@
 package com.kiodev.trailbadger.app;
 
+import java.util.Random;
+
 public class User {
     private String mName;
     private Double mFeet;
+    private Integer mImgResId;
+
+    private static final int[] mLeaderImages = {
+            R.drawable.john,
+            R.drawable.kelly,
+            R.drawable.ryan,
+            R.drawable.wally
+    };
 
     public User(String name, Double ft) {
         mName = name;
         mFeet = ft;
+
+        Random randomGenerator = new Random();
+        mImgResId =mLeaderImages[randomGenerator.nextInt(4)];
     }
 
     public String getName() {
@@ -23,5 +36,13 @@ public class User {
 
     public void setFeet(Double ft) {
         this.mFeet = ft;
+    }
+
+    public Integer getImgResId(){
+        return mImgResId;
+    }
+
+    public void setImgResId(Integer id){
+        mImgResId = id;
     }
 }
